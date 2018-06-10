260dbb38c8be1be4257c52d4b6fcfbc42307a777
Nikki Apostolakis <nikki.apostolakis@gmail.com>
2018-06-10 23:31:05 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -2,20 +2,30 @@
 require 'nokogiri'
 require 'pry'
 
+# projects: kickstarter.css("li.project.grid_4")
+# title: project.css("h2.bbcard_name strong a").text
+# image link: project.css("div.project-thumbnail a img").attribute("src").value
+# description: project.css("p.bbcard_blurb").text
+# location: project.css("span.location-name").text
+# percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
+# tag on a .gsub("%", "").to_i to remove the percent sign and convert it into an integer.
+
+
 def create_project_hash
   # write your code here
   # This just opens a file and reads it into a variable
   html = File.read('fixtures/kickstarter.html')
   kickstarter = Nokogiri::HTML(html)
 
-  binding.pry
-  # projects: kickstarter.css("li.project.grid_4")q
-  # title: project.css("h2.bbcard_name strong a").text
-  # image link: project.css("div.project-thumbnail a img").attribute("src").value
-  # description: project.css("p.bbcard_blurb").text
-  # location: project.css("span.location-name").text
-  # percent funded: project.css("ul.project-stats li.first.funded strong").text
-
+  projects = {}
+   
+    # Iterate through the projects
+  kickstarter.css("li.project.grid_4").each do |project|
+    projects[project] = {}
+  end
+   
+    # return the projects hash
+    projects
 end
 
 create_project_hash
